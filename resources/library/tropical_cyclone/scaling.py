98e4856cc2709ae3031b466eddfa310373ec1927
massimilianofronza <maxfronza97@gmail.com>
2025-01-24 12:09:23 +0100

    -MF : fix for StandardScaler code

--- resources/library/tropical_cyclone/scaling.py
+++ resources/library/tropical_cyclone/scaling.py
@@ -24,16 +24,15 @@ class Scaler():
 
 class StandardScaler(Scaler):
     def __init__(self, 
-            mean_src: str, 
-            std_src: str, 
+            src: str,
             drivers: List[str],  
             dtype = torch.float32
         ) -> None:
         super().__init__()
-        self._mean_ds = xr.load_dataset(mean_src)[drivers]
-        self._std_ds = xr.load_dataset(std_src)[drivers]
-        self._mean = torch.as_tensor(self._mean_ds[drivers].to_array().data, dtype=dtype)
-        self._std = torch.as_tensor(self._std_ds[drivers].to_array().data, dtype=dtype)
+
+        self._scaler_ds = xr.load_dataset(src)
+        self._mean = torch.as_tensor(self._scaler_ds[[f'mean-{drv}' for drv in drivers]].to_array().data, dtype=dtype)
+        self._std = torch.as_tensor(self._scaler_ds[[f'std-{drv}' for drv in drivers]].to_array().data, dtype=dtype)
 
     def transform(self, tensor: torch.Tensor) -> torch.Tensor:
         scaled_tensor = ((tensor - self._mean) / self._std)
